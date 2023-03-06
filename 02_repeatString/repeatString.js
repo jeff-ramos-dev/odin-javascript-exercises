e0316c232655f7d84ffd5b7be19877dae154b13b
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2023-03-06 09:21:34 -0800

    Complete 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, repeatNum) {
+    let outputString = ""
+    if (repeatNum < 0) {
+        return "ERROR" 
+    }
+    for (let i = 0; i < repeatNum; i++) {
+        outputString += inputString
+    }
+    return outputString
 };
 
 // Do not edit below this line
