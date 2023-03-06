e52eb864c4d1e476869ba0fa162adb24410cb248
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2023-03-06 09:57:19 -0800

    Complete 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, target) {
+    let outputArray = inputArray
+    for (let i = 1; i < arguments.length; i++) {
+        if (outputArray.includes(arguments[i])) {
+            outputArray.splice(outputArray.indexOf(arguments[i]), 1)
+        }
+    }
+    return outputArray
 };
 
 // Do not edit below this line
