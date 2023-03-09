4fcbd8e56d7c2946cd488dd50908663bacb624bb
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2023-03-09 12:48:17 -0800

    Complete 12-findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const agedPeople = people.map(person => {
+        if (!person.yearOfDeath) {
+            age = new Date().getFullYear() - person.yearOfBirth
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth
+        }
+        return {
+            "age": age,
+            ...person
+        }
+    }) 
+    return agedPeople.reduce((oldest, currentPerson) => {
+        return currentPerson.age > oldest.age ? currentPerson : oldest
+    })
 };
 
 // Do not edit below this line
