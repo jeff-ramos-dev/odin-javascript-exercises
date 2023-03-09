b7d1430310a987d4247a86448f2753e3501480cd
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2023-03-09 10:52:40 -0800

    Complete 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,61 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  const length = numArray.length
+  if (!numArray) {
+    return 0
+  }
+  if (length === 1) {
+    return numArray[0]
+  }
+  let total = 0
+  for (let i = 0; i < length; i++) {
+    total += numArray[i] 
+  }
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  const length = numArray.length
+  if (!numArray) {
+    return 0
+  }
+  if (length === 1) {
+    return numArray[0]
+  } 
+  let total = 1
+  for (let i = 0; i < length; i++) {
+    total *= numArray[i] 
+  }
+  return total
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  return num ** pow
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  switch(num) {
+    case(0):
+      return 1
+    case(1):
+      return 1
+    default:
+      break;
+  }
+  let multiplier = num - 1
+  let total = num
+  while (multiplier > 0) {
+    total *= multiplier
+    multiplier--
+  }
+  return total
 };
 
 // Do not edit below this line
