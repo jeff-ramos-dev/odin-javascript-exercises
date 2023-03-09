09f66ab0e7d74ca017f6932c2037cb00de9fb5ff
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2023-03-09 12:05:17 -0800

    Complete 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS"
+    if (num === 0) return 0
+    let previousFibValue = 0
+    let currentFibValue = 1
+    let i = 1
+    while (i < num) {
+        const temp = currentFibValue
+        currentFibValue = previousFibValue + currentFibValue
+        previousFibValue = temp
+        i++
+    }
+    return currentFibValue
 };
-
+// fibonacci(1) == 1
+// fibonacci(2) == 1
+// fibonacci(3) == 2
+// fibonacci(4) == 3
+// fibonacci(5) == 5
+// fibonacci(6) == 8
 // Do not edit below this line
 module.exports = fibonacci;
